e55bd6efc4fe1b6927287ffbce617cf6da30f3c1
Jhonattan Diaz <jhonalex123@gmail.com>
2020-06-06 22:26:49 -0500

    Ejercicio 4

---
+++ ProyectoInicial/src/EjerciciosClase4/Pila.java
@@ -0,0 +1,38 @@
+package EjerciciosClase4;
+
+import java.util.Stack;
+
+public class Pila {
+	private static int cantidad;
+	private static int precio,total;
+
+	public static void main(String[] args) {
+		Stack<persona> pila = new Stack<persona>();
+		cantidad=(int) Math.floor(Math.random()*50+5);
+		for(int a =0;a< cantidad; a++) {
+			int valor = (int) Math.floor(Math.random()*60+5);
+			precio=calcularprecio(valor);
+			persona a1 = new persona(valor,precio);
+	        pila.push(a1); 
+	        System.out.println("Visitante número "+(a+1)+" tiene "+valor+" años y paga: $"+precio);
+		}
+        while (!pila.isEmpty()) { 
+            //System.out.println(pila.pop().getEdad());
+        	total=total+pila.pop().getPrecio();
+        }
+        System.out.println("El total de venta es: $"+total+ " para una cantidad de "+cantidad+ " de personas");
+	}
+
+	private static int calcularprecio(int valor) {
+		int precio=0;
+		if(valor>=5 && valor<=10) {
+			precio=5000;
+		}else if(valor>=11 && valor<=17) {
+			precio=7500;
+		}else if(valor>=18) {
+			precio=10000;
+		}
+		return precio;
+	}
+
+}
--- ProyectoInicial/src/EjerciciosClase4/persona.java
+++ ProyectoInicial/src/EjerciciosClase4/persona.java
@@ -2,16 +2,26 @@ package EjerciciosClase4;
 
 public class persona {
 	private int edad;
+	private int precio;
     
     /**
      * Constructor por defecto
      * @param edad
      */
-    public persona(int edad){
+    public persona(int edad ,int precio){
         this.edad=edad;
+        this.precio=precio;
     }
 
-    /**
+    public int getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+
+	/**
      * Devuelve la edad
      * @return Edad acutal
      */
